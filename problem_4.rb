bb7abf4007d74e9425a2cd9c1cd9d4490a8e0f4f
Oozwal <ujjwalthapa2307@ioe.edu.np>
2020-12-16 22:28:00 +0900

    not using ruby inbuilt function

--- problem_4.rb
+++ problem_4.rb
@@ -8,11 +8,55 @@
 # Input: convert_number(42, “decimal”, “binary”)
 # Output: 101010
 
-def convert_number(number,from,to)
-  if(from == "decimal" && to == "octal")
-    puts "#{number.to_s(8)}"
-  end
-  if(from == "decimal" && to == "binary")
-    puts "#{number.to_s(2)}"
+# def convert_number(number,from,to)
+#   if(from == "decimal" && to == "octal")
+#     puts "#{number.to_s(8)}"
+#   end
+#   if(from == "decimal" && to == "binary")
+#     puts "#{number.to_s(2)}"
+#   end
+# end
+
+class Converter
+  def convert_number(number, from, to)
+    if(from == "decimal" && to == "octal")
+      i = 0
+      octal_number = []
+      quotient = number
+      while(quotient != 0)
+        octal_number[i] = quotient % 8
+        i = i + 1
+        quotient = quotient/8
+      end
+      i = i - 1
+      while(i > -1)
+        print "#{octal_number[i]}"
+        i = i - 1
+      end
+      print "\n"
+    end
+
+    if(from == "decimal" && to == "binary")
+      i = 0
+      binary_number = []
+      quotient = number
+      while(quotient != 0)
+        binary_number[i] = quotient % 2
+        i = i + 1
+        quotient = quotient/2
+      end
+      i = i - 1
+      while(i > -1)
+        print "#{binary_number[i]}"
+        i = i - 1
+      end
+      print "\n"
+    end
   end
 end
+
+
+
+test = Converter.new
+test.convert_number(42, "decimal", "octal")
+test.convert_number(42, "decimal", "binary")
